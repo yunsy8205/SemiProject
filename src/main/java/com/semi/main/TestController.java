0f6d05a9e8ff34d2ad01c7d03da4f61d5dffe366
kimseongho <sungho9941@github.com>
2023-08-21 14:10:23 +0900

    0821 14:10

--- src/main/java/com/semi/main/TestController.java
+++ src/main/java/com/semi/main/TestController.java
@@ -1,7 +1,20 @@
 package com.semi.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/")
 public class TestController {
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() throws Exception{
+		
+		return "home";
+	}
 	
+
+
 
 }
